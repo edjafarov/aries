5cfd3f2e7dcc0e6fcb7bfc79ae25cc90797f7169
Eldar Djafarov <eldar@djafarov.com>
2011-08-26 20:27:20 +0000

    post-get filters killed, Post&Get normalization moved to dispatcher. Normalized params autobinded to controllers arguments

--- src/controllers/SimpleController.js
+++ src/controllers/SimpleController.js
@@ -1,3 +1,4 @@
+IO.import("./src/controllers/HalloWorldController");
 /**
  * Hello world Constructor, also could be used to initialize 
  * object before running controller.
@@ -18,11 +19,13 @@ SimpleController.prototype.homepage = function(request, response){
 
 
 /**
- *@RequestMapping(value="/details") 
+ *@RequestMapping(value="/details/{test}") 
  */
-SimpleController.prototype.details = function(request, response){
+SimpleController.prototype.details = function(request, test , response, foo){
     this.setView("ejsView.html");
-    request.someVar = "here are details";
+    request.someVar = "Hi there. You are on a <b>details page</b> <br/>";
+    request.someVar += "in <b>" +  test + "</b> section <br/>";
+    request.someVar += "with foo param set to <b>" +  foo +"</b>";
     this.doNext(request,response);    
 };
 
