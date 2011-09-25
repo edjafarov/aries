37e328cd9ea21229bf7648eafe2f7ca3839dcf2a
Eldar Djafarov <djkojb@gmail.com>
2011-09-25 18:46:00 +0300

    #23|make project as npm module 2

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
 #!/usr/bin/env node
 var CONTROLLERS_PATH = "./controllers/";
-var APP_CFG_PATH = "./src/app-cfg/";
+
+var SRC_PATH = "./src";
+var PORT = process.env.PORT || process.env.npm_package_config_port;
+var HOST = process.env.npm_package_config_host || "";
+process.argv.forEach(function (val, index, array) {
+  if(val.indexOf("src=") != -1){
+	SRC_PATH = val.slice(val.indexOf("src=")+"src=".length);
+  }
+  if(val.indexOf("port=") != -1){
+	PORT = val.slice(val.indexOf("port=")+"port=".length);
+  }
+  if(val.indexOf("host=") != -1){
+	HOST = val.slice(val.indexOf("host=")+"host=".length);
+  }
+
+});
 
 
 var http = require('http');
@@ -11,7 +26,9 @@ var vm = require('vm');
 var eventsModule = require('events');
 var querystring = require('querystring');
 var logger = require('log4js').getLogger();
+var path = require('path');
 
+SRC_PATH = fs.realpathSync(SRC_PATH);
 /**
  *Read configuration:
  * @params
@@ -20,7 +37,7 @@ var logger = require('log4js').getLogger();
  * urlMapping [Array<String>] array of url mapping filenames 
  */
  
-CFG = require("./lib/ConfigReader.js")(APP_CFG_PATH);
+CFG = require("./lib/ConfigReader.js")(SRC_PATH);
 
 logger.log(util.inspect(CFG), false, 7);
 
@@ -42,11 +59,11 @@ var ClassLoader=require(CFG.ClassLoader);
 /**
  * Set Up Url resolver
  */
-var UrlResolver = ClassLoader.getClass(CFG.UrlResolver,{CFG:CFG, console:console, util:util, require:require})();
+var UrlResolver = ClassLoader.getClass(path.join(__dirname ,CFG.UrlResolver),{CFG:CFG, console:console, util:util, require:require})();
 
 
 console.log("Set up flow dispatcher...");
-var FlowDispatcher=ClassLoader.getClass(CFG.Dispatcher,{
+var FlowDispatcher=ClassLoader.getClass(path.join(__dirname ,CFG.Dispatcher),{
         console:logger,
         vm:vm,
         require:require,
@@ -68,6 +85,6 @@ var flowDispatcher = new FlowDispatcher(CFG);
 
  http.createServer(function (request, response) {
 	flowDispatcher.dispatch(request, response);
- }).listen(process.env.PORT || process.env.npm_package_config_port);
+ }).listen(PORT);
 
- console.log('Aries server running at http://'+ process.env.npm_package_config_host+':'+process.env.npm_package_config_port);
+ console.log('Aries server running at http://'+ HOST+':'+PORT);
