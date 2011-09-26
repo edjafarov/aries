7b123508425d5d05991679502b7ccfa21b8ce0bf
Eldar Djafarov <djkojb@gmail.com>
2011-09-27 00:30:54 +0300

    example controllers update

--- src/controllers/HalloWorldController.js
+++ src/controllers/HalloWorldController.js
@@ -15,18 +15,20 @@ HalloWorldController.prototype.sayHallo = function(request, response){
 	response.end("Hello world!");
 };
 
+
+HalloWorldController.prototype.commonModel = {
+	head:{
+		title:"aries-node: {hallo there!}"
+	},
+	topNavBar:{activeItem:null}
+}
+
 /**
  *@RequestMapping(value="/twitter") 
  */
 HalloWorldController.prototype.twitter = function(request, response){
-    this.setView("/ejs/Layout.html",
-	{
-	head:{
-		title:"aries-node: {hallo there!}"
-		},
-	bodyPath:"/ejs/twitterExample.html",
-	topNavBar:{activeItem:null}
-	});	
+	this.commonModel.bodyPath = "/ejs/twitterExample.html";
+	this.setView("/ejs/Layout.html", this.commonModel);	
 	this.doNext(request, response);
 };
 
@@ -39,17 +41,11 @@ HalloWorldController.prototype.twitterFeed = function(request, response, twitId)
 
 	doTwitterCall(twitId, function(msg){
 		var twitResponse = JSON.parse(msg);
-				
-		that.setView("/ejs/Layout.html",
-		{
-		head:{	
-			title:"aries-node: {hallo "+ twitId +"!}"
-			},
-		bodyPath:"/ejs/twitterFeedExample.html",
-		topNavBar:{activeItem:null},
-		twitResponse:twitResponse,
-		userId:twitId
-		});	
+		that.commonModel.head.title = "aries-node: {hallo "+ twitId +"!}";
+		that.commonModel.bodyPath = "/ejs/twitterFeedExample.html";
+		that.commonModel.twitResponse = twitResponse;
+		that.commonModel.userId = twitId;
+		that.setView("/ejs/Layout.html", that.commonModel);
 		that.doNext(request, response);				
 	})
 
@@ -64,17 +60,11 @@ HalloWorldController.prototype.twitterUser = function(request, response, userId)
 
 	doTwitterCall(userId, function(msg){
 		var twitResponse = JSON.parse(msg);
-				
-		that.setView("/ejs/Layout.html",
-		{
-		head:{	
-			title:"aries-node: {hallo "+ userId +"!}"
-			},
-		bodyPath:"/ejs/twitterFeedExample.html",
-		topNavBar:{activeItem:null},
-		twitResponse:twitResponse,
-		userId:userId
-		});	
+		that.commonModel.head.title = "aries-node: {hallo "+ userId +"!}";
+		that.commonModel.bodyPath = "/ejs/twitterFeedExample.html";
+		that.commonModel.twitResponse = twitResponse;
+		that.commonModel.userId = userId;
+		that.setView("/ejs/Layout.html", that.commonModel);	
 		that.doNext(request, response);				
 	})
 
